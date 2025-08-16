e4f5bcaa153cdec482e8e0595ae4ce46e80472c6
tvanjak <toni.vanjak@gmail.com>
2025-08-16 15:01:08 +0200

    Typo fix

--- TMDBApp/TMDBApp/ViewModels/MovieViewModel.swift
+++ TMDBApp/TMDBApp/ViewModels/MovieViewModel.swift
@@ -21,12 +21,12 @@ class MovieViewModel: ObservableObject {
         }
     }
     
-    func loadPopularMovies() {
-        TMDBService.shared.fetchPopularMovies { [weak self] result in
+    func loadTrendingMovies() {
+        TMDBService.shared.fetchTrendingMovies { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let movies):
-                    self?.popularMovies = movies
+                    self?.trendingMovies = movies
                 case .failure(let error):
                     self?.errorMessage = error.localizedDescription
                 }
